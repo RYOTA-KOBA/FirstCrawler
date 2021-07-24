dee495369fd5a33be7a5b56346e5042ec719202b
RYOTA <kobaryo04ysh@icloud.com>
2021-07-25 00:17:13 +0900

    fix: envファイルからurlを取得

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 from bs4 import BeautifulSoup
 import urllib.request
+from dotenv import load_dotenv
+import os
 
-res = urllib.request.urlopen('https://suumo.jp/jj/chintai/ichiran/FR301FC005/?shkr1=03&shkr3=03&cb=0.0&shkr2=03&smk=&mt=9999999&sc=13103&ar=030&bs=040&shkr4=03&ct=15.0&co=1&co=3&cn=5&ta=13&mb=0&fw2=&et=9999999')
+load_dotenv()
+
+suumo_url = os.getenv('SUUMO_URL')
+
+res = urllib.request.urlopen(suumo_url)
 
 name = ''
 fee = ''
+address = ''
 
 soup = BeautifulSoup(res.read(), 'html.parser')
 contents = soup.find_all('div', class_='property')
@@ -16,5 +23,9 @@ for content in contents:
 
     if content.select('div.detailbox-property-point'):
         fee = content.select('div.detailbox-property-point')[0].get_text()
+    
+    if content.select('td.detailbox-property-col'):
+        address = content.select('td.detailbox-property-col')[4].get_text().strip()
 
-    print(name + ":" + fee)
+
+    print("場所" + address + " " + name + "  家賃" + fee)
