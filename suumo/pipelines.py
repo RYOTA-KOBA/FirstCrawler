ad92ea5a3594b9d86d982fcbc24bc566d9bff7c1
RYOTA <kobaryo04ysh@icloud.com>
2021-07-25 18:57:07 +0900

    feat: CSVファイルへの書き出しを設定

--- suumo/pipelines.py
+++ suumo/pipelines.py
@@ -6,8 +6,25 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from datetime import datetime
+import csv
 
 
 class SuumoPipeline:
+
+    def open_spider(self, spider):
+        f = open(f'{datetime.now().strftime("%Y-%m-%d-%H-%M-%s")}.csv', 'w')
+        fieldnames =  [
+            'name',
+            'fee',
+            'address'
+        ]
+        self.f = f
+        self.writer = csv.DictWriter(f, fieldnames=fieldnames)
+        self.writer.writeheader()
+    
+    def close_spider(self, spider):
+        self.f.close()
+
     def process_item(self, item, spider):
-        return item
+        self.writer.writerow(ItemAdapter(item).asdict())
